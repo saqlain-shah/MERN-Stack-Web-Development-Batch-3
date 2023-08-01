a8d687dfc9e2c639b843903e8ddb7e94118ad213
Muhammad Rafiq <sameerbalti704@gmail.com>
2023-08-01 18:22:09 +0500

    javascipt code

---
+++ javascript/program.js
@@ -0,0 +1,17 @@
+//values overwrite
+var a = 3;
+var a = 5;
+//  console.log("value of a:", a)
+ // data types
+ const name = "Rafiq";
+ var age = 25;
+ let gender = "male";
+ let location = "";
+ var address = ;
+ console.log("location is :", location);
+ console.log("address is :", address);
+ // const values are not changed
+//  const user = "this is a user";
+//  const user = " this is a user 2";
+//  console.log("user is:", user);
+ //
