cb4b39ba0425591d66cc7b8eb32c17f4d89b5e1d
Muhammad Rafiq <sameerbalti704@gmail.com>
2023-08-02 11:50:17 +0500

    array and objects and nested objects

--- javascript/program.js
+++ javascript/program.js
@@ -7,11 +7,36 @@ var a = 5;
  var age = 25;
  let gender = "male";
  let location = "";
- var address = ;
- console.log("location is :", location);
- console.log("address is :", address);
+//  var address = "";
+//  console.log("location is :", location);
+//  console.log("address is :", address);
  // const values are not changed
 //  const user = "this is a user";
 //  const user = " this is a user 2";
 //  console.log("user is:", user);
- //
+ //array and object
+ var emlpoyee = ["rafiq", 25 , "ics", "skardu"];
+ console.log("array values:", emlpoyee);
+
+let obj2 = {
+    emlpoyee: "atif",
+    id: "31A",
+    status: "HR",
+    company: "rinor technologies"
+}
+ 
+ var obj1 ={
+    name: "muhammad rafiq",
+    age: 25,
+    address: "skardu",
+    gender: "male",
+    emlpoyee : ["rafiq", 25 , "ics", "skardu"],
+    obj2 : {
+        emlpoyee: "atif",
+        id: "31A",
+        status: "HR",
+        company: "rinor technologies"
+    }
+
+ }
+ console.log("values of objects:", obj1.obj2.company);
