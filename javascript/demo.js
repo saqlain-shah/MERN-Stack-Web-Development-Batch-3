4398bdc424ddb987a843a0ac42073c324fc7acaf
Muhammad Iqbal <iqbalkainat@gmail.com>
2023-08-03 06:03:00 -0700

    function

--- javascript/demo.js
+++ javascript/demo.js
@@ -3,6 +3,8 @@ let b=10;
 if (a===b){
     console.log(true);
 }
+
 else{
+    
     console.log(false);
 }
---
+++ javascript/function.js
@@ -0,0 +1,8 @@
+
+let a=10;
+let b=15;
+
+function add(a,b){
+    return (a+b)
+}
+console.log("add tow number :",add(a,b))
