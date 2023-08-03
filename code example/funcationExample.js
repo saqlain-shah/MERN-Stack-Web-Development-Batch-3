0657fefb1c15e94c4eb69b7901bddc8e13311b3d
Shahzaib Ali <sadiqsheen313@gmail.com>
2023-08-03 18:30:07 +0500

    Function

---
+++ code example/funcationExample.js
@@ -0,0 +1,12 @@
+//funcation
+let x = myFuncation(7,3)
+function myFuncation(a,b){
+    return a * b
+}
+
+let a=89;
+let b=94;
+function multiply(a,b){
+    return a*b
+}
+console.log(multiply(a,b) )
