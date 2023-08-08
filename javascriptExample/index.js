06ca292f4511001e9f9fd8b48988378c1577e366
Muhammad Iqbal <iqbalkainat@gmail.com>
2023-08-08 06:24:24 -0700

    indexof

--- javascriptExample/index.js
+++ javascriptExample/index.js
@@ -1,3 +1,3 @@
-let text = "Pleaseee locate where 'locate' occurs!";
+let text = "Please locate where 'locate' occurs!";
 let index = text.indexOf("locate");
 console.log(index)
---
+++ javascriptExample/indexof.js
@@ -0,0 +1,3 @@
+var text = "Please locate where 'locate' occurs!";
+var index  = text.indexOf("locate", 15);
+console.log(index);
