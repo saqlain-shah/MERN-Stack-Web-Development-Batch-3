05edc88cbd24ff25ac2a2224d5ecf9e1e5a41867
Shahzaib Ali <sadiqsheen313@gmail.com>
2023-08-08 18:27:55 +0500

    string.js

---
+++ code example/loopExample.js
@@ -0,0 +1,3 @@
+let a=9
+let b=8
+console.log(a + b)
---
+++ code example/string.js
@@ -0,0 +1,16 @@
+//stringslice
+let text = "Mango, apple, graphs";
+let part = text.slice(3,5)
+console.log(part)
+//second one
+let text1 = "Mango, apple, graphs";
+let part2 = text.slice(-10)
+console.log(part2)
+//third one
+let text3 = "Mango, apple, graphs";
+let part3 = text.slice(-8,-5)
+console.log(part3)
+///ffg
+let str = "Apple, Banana, Kiwi";
+let part4 = str.substring(7, 13);
+console.log(part4)
