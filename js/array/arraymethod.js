92b8214ee290d5076197e264e01e1abe54891521
waju-rizvi <wajurizvi@localhost.localdomain>
2023-08-10 18:39:02 +0500

    array methods|| waju rizvi

---
+++ js/array/arraymethod.js
@@ -0,0 +1,16 @@
+const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+
+const result = words.filter((word) => word.length > 6);
+
+console.log(result);
+// Expected output: Array ["exuberant", "destruction", "present"]
+const array1 = [1, 4, 9, 16];
+
+// Pass a function to map
+const map1 = array1.map((x) => x * 2);
+
+console.log(map1);
+// Expected output: Array [2, 8, 18, 32]
+const array2 = ['a', 'b', 'c'];
+
+array2.forEach((element) => console.log(element));
