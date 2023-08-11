8f2a0988864e84bf677285b1993cacc89ca872b9
waju-rizvi <wajurizvi@localhost.localdomain>
2023-08-11 18:35:43 +0500

    practice array

---
+++ js/array/arrayprac.js
@@ -0,0 +1,14 @@
+var array=[1,2,3,4]
+var map1 = array.map((x) => x !== 2);
+//console.log(map1);
+
+//var forEach1=array.forEach((elem , index) => `${elem} +'index no'+ ${index}`);
+//console.log(forEach1);
+
+
+let wajahat=['names','age','phone',['s.wajahat' , 18 , 035543]];
+//var forEach=wajahat.forEach((x) => console.log(x) )
+var filter3=wajahat.map((element, index ) => console.log(element.length<3) )
+
+// console.log(wajahat[3][1])
+
