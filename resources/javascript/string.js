9a7223785d8ea6c6f5fb9b456fc3351be6dafd61
Dilshad Hussain <dilshad9930@gmail.com>
2023-08-12 16:34:52 +0500

    string.js

--- resources/javascript/string.js
+++ resources/javascript/string.js
@@ -1,3 +1,7 @@
 let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let length = text.length;
-console.log(length)
+console.log(length);
+
+let text1 = "Apple, Banana, Kiwi";
+let part = text1.slice(7, 13);
+console.log(part);
