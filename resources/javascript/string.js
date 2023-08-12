a4e9fe3284de9d3e78e33253cf2286b04c8a7cd5
Dilshad Hussain <dilshad9930@gmail.com>
2023-08-12 16:44:16 +0500

    string.js

--- resources/javascript/string.js
+++ resources/javascript/string.js
@@ -5,3 +5,7 @@ console.log(length);
 let text1 = "Apple, Banana, Kiwi";
 let part = text1.slice(7, 13);
 console.log(part);
+
+let text2 = "Apple, Banana, Kiwi";
+let part1 = text2.slice(7);
+console.log(part1)
