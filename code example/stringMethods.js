2c2788e91353f497f584b39c232d29bd14cbf3ce
Shahzaib Ali <sadiqsheen313@gmail.com>
2023-08-16 17:31:40 +0500

    file edit

---
+++ code example/stringMethods.js
@@ -0,0 +1,15 @@
+// concat methods
+let num = [1, 2, 3, 4,]
+let num_more = [11, 12, 13, 14,]
+let newArray = num.concat(num_more)
+
+// sort methods
+
+let num1 = [33, 34, 55, 66,]
+num.sort ()
+console.log(num1)
+
+// splice and slice
+let num3 = [3, 33, 333, 333,]
+num.splice(1, 2,222, 2222, )
+console.log(num3)
---
+++ resources/program.js
@@ -0,0 +1,5 @@
+let fruits = ["Apple", "Orange", "Plum"];
+
+console.log( fruits[0] ); // Apple
+console.log( fruits[1] ); // Orange
+console.log( fruits[2] ); // Plum
