9198ac35660091fc8874676550da94c68753d76a
Kainat1234 <iqbalkainatt@gmail.com>
2023-08-19 07:45:39 -0700

    using id and class

---
+++ DOM Project1/javascript.js
@@ -0,0 +1,8 @@
+let ctitle = document.getElementById("first")
+ctitle.style.color = "blue"
+
+let ctitles = document.querySelectorAll(".card-title")
+
+ctitles[0].style.color = "red"
+ctitles[1].style.color = "green"
+ctitles[2].style.color = "blue"
