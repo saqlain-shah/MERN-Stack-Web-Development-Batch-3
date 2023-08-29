789e1b4d358922f34d50c696590c6b57e6ea42ed
Saleem Abbas <saleemabbas1998@gmail.com>
2023-08-29 18:01:14 +0500

    Streak

---
+++ my-react-app/src/components/Button.jsx
@@ -0,0 +1,7 @@
+ function MyButton() {
+    return (
+      <button>I'm a button</button>
+    );
+  }
+
+export default MyButton;
---
+++ my-react-app/src/components/Profile.jsx
@@ -0,0 +1,24 @@
+import Img from '../Asset/Saleem_Picture.jpeg'
+const user = {
+    name: 'Hedy Lamarr',
+    imageUrl: Img,
+    imageSize: 90,
+  };
+  
+  export default function Profile() {
+    return (
+      <>
+        <h1>{user.name}</h1>
+        <img
+          className="avatar"
+          src={user.imageUrl}
+          alt={'Photo of ' + user.name}
+          style={{
+            width: user.imageSize,
+            height: user.imageSize
+          }}
+        />
+      </>
+    );
+  }
+  
