42c5719ee7701b2ae5df68af337c7ef181672260
waju-rizvi <wajurizvi@localhost.localdomain>
2023-08-30 18:06:04 +0500

    react first pro|| waju rizvi

---
+++ demo-app/src/components/profile.jsx
@@ -0,0 +1,22 @@
+const user = {
+    name: 'Hedy Lamarr',
+    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+    imageSize: 90,
+  };
+  
+  export default function Profile() {
+    return (
+      <>
+        <h1>{user.name}</h1>
+        <img
+          className="avatar"
+          src={user.imageUrl}
+          alt={'Photo of ' + user.name}
+          style={{
+            width: user.imageSize,
+            height: user.imageSize
+          }}
+        />
+      </>
+    );
+  }
