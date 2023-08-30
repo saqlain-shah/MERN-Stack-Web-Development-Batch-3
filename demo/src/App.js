97421e4e0d334fe4cfaf4ffd4b0d69a7e356cee4
Suhail Yousuf <suhaily312@gmail.com>
2023-08-30 06:49:29 -0700

    react file added

---
+++ demo/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Profile from "./component/Profile";
+import  Product from "./component/Product"
+import A from "./component/pro"
+export default function App() {
+  return (
+    <>
+      <Profile />
+      <Product />
+      <A />
+    </>
+  );
+}
