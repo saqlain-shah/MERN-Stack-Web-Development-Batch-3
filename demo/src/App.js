3f9878ea6baea8daee8af20c3650821e7f7bebc9
Suhail Yousuf <suhaily312@gmail.com>
2023-08-31 07:27:41 -0700

    new File

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import Profile from "./component/Profile";
-import  Product from "./component/Product"
-import A from "./component/pro"
+//import Profile from "./component/Profile";
+//import  Product from "./component/Product";
+//import A from "./component/pro";
+import Buttom from "./component/Counter"
 export default function App() {
   return (
     <>
-      <Profile />
-      <Product />
-      <A />
+       <Buttom/>
+ 
     </>
   );
 }
