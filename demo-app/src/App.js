77be88c4fb18311877ed38ade6ddfd4287b78734
waju-rizvi <wajurizvi@localhost.localdomain>
2023-08-31 17:16:04 +0500

    first

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,9 +1,11 @@
-
+import naav from './components/nav'
 import Profile from './components/profile'
 export default function app() {
   return(
     <>
+    <naav></naav>
     <Profile></Profile>
+    
     </>
   )
   
---
+++ demo-app/src/components/nav.jsx
@@ -0,0 +1,14 @@
+const title="home";
+export default function nav()
+{
+    return(
+        <>
+        <nav>
+            <li>{title}</li>
+            <li>contant</li>
+            <li>profile</li>
+        </nav>
+        </>
+    );
+    
+}
