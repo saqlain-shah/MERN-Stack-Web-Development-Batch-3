769c35d226e658c7c95287b7b7bb9c7437c9c68c
Suhail Yousuf <suhaily312@gmail.com>
2023-09-06 06:30:20 -0700

    file add

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+//import Main from "./components/Main";
+//import Profile from "./components/Profile.jsx";
+import Counter from "./components/Counter";
+import LoginForm from "./components/Form";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <LoginForm />
+
+      {/*   
+         <Counter/>
+      <Profile />
+      <Main />
+      */}
+    </>
   );
 }
-
-export default App;
