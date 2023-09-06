2c8b3cc701dd02b567a831a44f7dbec41baad596
waju-rizvi <wajurizvi@localhost.localdomain>
2023-09-06 18:55:34 +0500

    changes

---
+++ demo-app/src/components/form.jsx
@@ -0,0 +1,26 @@
+import React,{useState} from 'react'
+
+
+export default function form() {
+  
+  return (<> 
+   <label htmlFor="">username</label><br />
+    <input 
+    type="text" 
+    name=""
+     id="username" 
+    onChange={} /><br />
+    <label htmlFor="">password</label><br />
+    <input 
+    type="password" 
+    name="" 
+    id="" 
+    /><br />
+    <label htmlFor="">email</label><br />
+    <input type="email" name="" id="email" />
+    <button></button>
+
+    </>
+  
+  )
+}
