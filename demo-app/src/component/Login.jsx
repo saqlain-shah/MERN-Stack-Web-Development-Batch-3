b48cbf8f01626cfd49cef9e995f284e39cc33587
Shahzaib Ali <sadiqsheen313@gmail.com>
2023-09-09 08:12:29 +0500

    add new files

--- demo-app/src/component/Login.jsx
+++ demo-app/src/component/Login.jsx
@@ -1,43 +1,49 @@
-import React, { useState } from 'react'
+// SignIn.js
+import React, { useState } from 'react';
 
-export default function Login() {
-    const[value, setvalue]= useState({
+function SignIn() {
+    const [signInData, setSignInData] = useState({
         email: '',
-        pasowrd:''
-    })
-    function userLoginData(data){
-    const{name, value} =data.target;
-    setvalue((prevData) => ({
-        ...prevData,
-        [name] :value
-    }))
-    
-  }
-  function userLoginsubmit(){
-    alert(`your email is ${value.email} and password is ${value.password}`)
-  }
-  return (
-    <div>
-      <form className='loginForm'>
-        <h2>Login form</h2>
-        <label htmlFor="Userna me"></label>
-        <input
-        type="text"
-        name="email"
-        value={value.email}
-        onChange={userLoginData}
-        placeholder ="Enter user email" 
-        />
-        <label htmlFor="Username"></label>
-        <input
-        type="text"
-        name="password"
-        value={value.password}
-        onChange={userLoginData}
-        placeholder= "Enter user password"  
-        />
-        <button onClick={userLoginsubmit}>Login</button>
-      </form>
-    </div>
-  )
+        password: '',
+    });
+
+    const handleSignIn = () => {
+        console.log('Sign-In Data:', signInData);
+    };
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setSignInData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    return (
+        <div>
+            <h2>Sign In</h2>
+            <form>
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    value={signInData.email}
+                    onChange={handleInputChange}
+                    
+                />
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    value={signInData.password}
+                    onChange={handleInputChange}
+                />
+                <button type="button" onClick={handleSignIn}>
+                    Sign In
+                </button>
+            </form>
+        </div>
+    );
 }
+
+export default SignIn;
