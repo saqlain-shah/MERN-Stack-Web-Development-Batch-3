c3797773eba1b2cc76b5a9bd51d7c58f31f6dcfe
saqlain shah <cyedcaqlain1@gmail.com>
2023-09-20 19:01:25 +0500

    integrated endpoint to display multiple product

--- demo-app/src/components/others/ProductCard.jsx
+++ demo-app/src/components/others/ProductCard.jsx
@@ -7,7 +7,7 @@ import ProgressLoading from "./ProgressLoading"
 const ProductCard = () => {
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(null)
-    const [data, setData] = useState({
+    const [data, setData] = useState([{
         id: '',
         title: '',
         description: '',
@@ -19,13 +19,22 @@ const ProductCard = () => {
         category: '',
         thumbnail: '',
         images: '',
-    })
+    }])
 
-    useEffect( () => {
-        axios.get('https://dummyjson.com/products/')
+    const cardStyle = {
+        display: 'inline-block', // Display cards in a row
+        maxWidth: '300px',       // Set maximum card width
+        margin: '8px',           // Adjust margin to create space between cards
+        border: '1px solid #ddd',
+        padding: '16px',
+    };
+
+    useEffect(() => {
+        axios.get('https://dummyjson.com/products/?limit=0')
             .then(response => {
                 setData(response.data)
                 setLoading(false)
+                console.log("state variable value", response.data)
 
             })
             .catch(err => {
@@ -45,54 +54,66 @@ const ProductCard = () => {
                 <p>Error: {error.message}</p>
             ) : data ? (
 
-                <Card sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', maxWidth: '300px', margin: 'auto', border: '1px solid #ddd', padding: '16px' }}>
-                    <CardMedia
-                        component="img"
-                        alt={data.title}
-                        height="200"
-                        image={data.thumbnail}
-                    />
-                    <CardContent sx={{ textAlign: 'center' }}>
-                        <Typography variant="h6" component="div">
-                            {data.title}
-                        </Typography>
-                        <Typography variant="subtitle1" color="text.secondary">
-                            {data.description}
-                        </Typography>
-                        <Typography variant="h6" color="text.primary">
-                            ${data.price}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            {data.discountPercentage}% off
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary" sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                            <StarIcon color="primary" sx={{ marginRight: '4px' }} />
-                            {data.rating}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            In Stock: {data.stock} items
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            Brand: {data.brand}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            Category: {data.category}
-                        </Typography>
-                    </CardContent>
-                    <div sx={{ display: 'flex', justifyContent: 'center', marginTop: '16px' }}>
-                        {data.images && data.images.map((image, index) => (
-                            <img
-                                key={index}
-                                src={image}
-                                alt={`Product ${index + 1}`}
-                                style={{ width: '80px', marginRight: '8px', marginBottom: '8px' }}
-                            />
-                        ))}
-                    </div>
-                    <Button variant="contained" color="primary" sx={{ marginTop: '16px' }}>
-                        Add to Cart
-                    </Button>
-                </Card>
+                        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+
+            {
+                        data.products.map((item,index ) => (
+                            <Card key={index} style={cardStyle}
+                            sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', maxWidth: '300px', margin: 'auto', border: '1px solid #ddd', padding: '16px' }}>
+                                <CardMedia
+                                    component="img"
+                                    alt={item.title}
+                                    height="200"
+                                    image={item.thumbnail}
+                                />
+                                <CardContent sx={{ textAlign: 'center' }}>
+                                    <Typography variant="h6" component="div">
+                                        {index+1}
+                                    </Typography>
+                                    <Typography variant="h6" component="div">
+                                        {item.title}
+                                    </Typography>
+                                    <Typography variant="subtitle1" color="text.secondary">
+                                        {item.description}
+                                    </Typography>
+                                    <Typography variant="h6" color="text.primary">
+                                        ${item.price}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        {item.discountPercentage}% off
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary" sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                                        <StarIcon color="primary" sx={{ marginRight: '4px' }} />
+                                        {item.rating}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        In Stock: {item.stock} items
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        Brand: {item.brand}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        Category: {item.category}
+                                    </Typography>
+                                </CardContent>
+                                <div sx={{ display: 'flex', justifyContent: 'center', marginTop: '16px' }}>
+                                    {item.images && item.images.map((image, index) => (
+                                        <img
+                                            key={index}
+                                            src={image}
+                                            alt={`Product ${index + 1}`}
+                                            style={{ width: '80px', marginRight: '8px', marginBottom: '8px' }}
+                                        />
+                                    ))}
+                                </div>
+
+                            </Card>
+                            
+                        ))
+
+                                    }
+                        </div>
+
             ) : null}
 
 
