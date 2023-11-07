4c7f961c57c23db529962ec905bffcb4ed1c80f3
muhammadalee77 <muhammadalidumsumi555@example.com>
2023-11-07 15:52:49 +0500

    new controller added

--- backend/Routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { Register , Login  } from "./authRoutes"
-
-
-const router = express.Router();
-router.post("/register", register);
-router.post("/login", login);
-
-export default router;
